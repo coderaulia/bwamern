a1718e7dd59ddff7302d0751b63f169cf03b8edd
Aulia Satrio <auliasw89@gmail.com>
2021-03-20 23:59:06 +0700

    testing button 1

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Button from "./index";
+
+test("Should not allowed click button if isDisabled is present", () => {
+	const { container } = render(<Button isDisabled></Button>);
+	expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("Should render loading/spinner", () => {
+	const { container, getByText } = render(<Button isLoading></Button>);
+
+	expect(getByText(/loading/i));
+
+	expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
