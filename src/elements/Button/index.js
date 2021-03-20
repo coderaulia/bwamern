bbe27d8631a6059b510d6b352346af22efecf551
Aulia Satrio <auliasw89@gmail.com>
2021-03-21 00:12:46 +0700

    finishing button testing

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -22,7 +22,7 @@ export default function Button(props) {
 				{props.isLoading ? (
 					<>
 						<span className="spinner-border spinner-border-sm mx-5"></span>
-						<span className="sr-only"></span>
+						<span className="sr-only">Loading...</span>
 					</>
 				) : (
 					props.children
