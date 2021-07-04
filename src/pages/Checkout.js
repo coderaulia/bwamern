d47d3cb74c2e650401021553a3f6656f0c9888c3
coderaulia <auliasw89@gmail.com>
2021-07-04 20:55:39 +0700

    start again, checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
 
 import Header from "parts/Header";
+import Fade from "react-reveal/Fade";
+
+import Button from "elements/Button";
+import Stepper, {
+	Numbering,
+	Meta,
+	MainContent,
+	Controller,
+} from "elements/Stepper";
+
+import BookingInformation from "parts/Checkout/BookingInformation";
+import Payment from "parts/Checkout/Payment";
+import Completed from "parts/Checkout/Completed";
+
+import ItemDetails from "json/itemDetails.json";
 
 export default class Checkout extends Component {
 	render() {
